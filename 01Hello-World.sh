6d0adbc9d5964b6f45059d4f863aff6f2cdb37bd
root@ip-172-31-28-130.us-west-2.compute.internal <root@ip-172-31-28-130.us-west-2.compute.internal>
2024-04-09 14:41:28 +0000

    updated

---
+++ 01Hello-World.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hello World, I am learning DevOps with AWS"
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Hi Ramesh, Good Morning"
+echo "Ramesh: Hi Suresh, How are you doing"
+echo "Suresh: Hi Ramesh, I am doing good"
