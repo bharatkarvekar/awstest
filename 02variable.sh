cdb681073f299e76b3bd421b3dccbcbffc555ddf
root@ip-172-31-28-130.us-west-2.compute.internal <root@ip-172-31-28-130.us-west-2.compute.internal>
2024-04-10 07:35:03 +0000

    02 sh added

---
+++ 02variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bashi
+echo "Hello World, I am learning DevOps with AWS"
+
+PERSON1=sachin
+PERSON2=Mohand
+echo "$PERSON1: Hi $PERSON2 Good Morning"
+echo "$PERSON2 Hi $PERSON1, Good Morning"
